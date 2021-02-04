1bab8457faea0784249b038866a2568b4d0e5c43
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2021-02-04 15:04:41 +0300

    feat(team): implement createTeam and updateTeam mutations

--- src/teams/services/teams.service.ts
+++ src/teams/services/teams.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { TeamEntity } from '../models/team.entity';
+import { ICreateTeam, IUpdateTeam } from '../models/team.interface';
+import { getNextTeamNumber } from '../utils/get-next-team-number.util';
 
 @Injectable()
 export class TeamsService {
@@ -19,6 +21,25 @@ export class TeamsService {
   }
 
   public findByIds(ids: string[]): Promise<TeamEntity[]> {
-    return this.teamsRepository.findByIds(ids, { loadRelationIds: true });
+    return this.teamsRepository.findByIds(ids, { loadRelationIds: true, order: { number: 'ASC' } });
+  }
+
+  public async createTeam(props: ICreateTeam): Promise<TeamEntity> {
+    const teams: TeamEntity[] = await this.teamsRepository.find({ courseId: props.courseId });
+    const nextNumber: number = getNextTeamNumber(teams);
+
+    const team: TeamEntity = this.teamsRepository.create({
+      ...props,
+      password: Math.random().toString(36).substr(2, 8),
+      number: nextNumber,
+    });
+
+    return this.teamsRepository.save(team);
+  }
+
+  public async updateTeam(props: IUpdateTeam): Promise<TeamEntity> {
+    await this.teamsRepository.update(props.id, { tgLink: props.tgLink });
+
+    return this.teamsRepository.findOne(props.id);
   }
 }
